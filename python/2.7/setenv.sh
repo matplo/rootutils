6b8ba510785058a0ea935e52099bcd359186ca0c
matplo <ploskon@gmail.com>
2014-02-16 01:00:08 +0100

    getting rid of the relative path remains - thanks to: http://stackoverflow.com/questions/3572030/bash-script-absolute-path-with-osx

--- python/2.7/setenv.sh
+++ python/2.7/setenv.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-THISFILE="$BASH_SOURCE"
+function abspath()
+{
+  case "${1}" in
+    [./]*)
+    echo "$(cd ${1%/*}; pwd)/${1##*/}"
+    ;;
+    *)
+    echo "${PWD}/${1}"
+    ;;
+  esac
+}
+
+THISFILE=`abspath $BASH_SOURCE`
 XDIR=`dirname $THISFILE`
 if [ -L ${THISFILE} ];
 then
