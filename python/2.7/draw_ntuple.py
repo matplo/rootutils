cb71a28254dd928fd819340c4942d745104bc0c8
ploskon <ploskon@gmail.com>
2016-02-08 18:10:48 +0100

    2D draw tuple

--- python/2.7/draw_ntuple.py
+++ python/2.7/draw_ntuple.py
@@ -103,3 +103,69 @@ def h1d_from_ntuple_dir_filter(cdir, ntname, var, cuts, bwidth, xlow, xhigh, tit
     lstore.draw_all(logy=True)
     return hlret
 
+### 2DIM
+
+def h2d_from_ntuple(fname, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title=None, modname='', nev=-1):
+    nbinsx = int((xhigh-xlow)/bwidth*1.)
+    if nbinsx < 1:
+        return None
+    nbinsy = int((yhigh-ylow)/ybwidth*1.)
+    if nbinsy < 1:
+        return None
+    hname = ut.build_string([fname, ntname, var, cuts, xlow, xhigh, ylow, yhigh],'-')
+    hname = hname.replace('/', '.')
+    hname = hname.replace('*', 'x')
+    hname = hname.replace('(', '.')
+    hname = hname.replace(')', '.')
+    if len(modname) > 0:
+        hname = modname
+    if title==None:
+        htitle = ut.build_string([fname, var],' ')    
+    else:
+        htitle = title
+    hret = None
+    try:
+        fin = ROOT.TFile(fname)
+        if fin:
+            tn = fin.Get(ntname)
+            if tn:
+                hname_tmp = 'htmp({0},{1},{2},{3},{4},{5})'.format(nbinsx, xlow, xhigh, nbinsy, ylow, yhigh)
+                dstr = '{0}>>{1}'.format(var, hname_tmp)
+                if nev > 0:
+                    dentries = tn.Draw(dstr, cuts, 'e', nev) #call sumw2 before the histogram creation!
+                else:
+                    dentries = tn.Draw(dstr, cuts, 'e') #call sumw2 before the histogram creation!
+                hret = ROOT.gDirectory.Get('htmp')
+                hret.SetDirectory(0)
+            else:
+                print '[e] no ntuple:',ntname
+            fin.Close()
+        else:
+            print '[e] no root file:',fname
+        hret.SetName(hname)
+        hret.SetTitle(htitle)
+    except:
+        print >> sys.stderr,'[e] draw from ntuple failed [{}:{}:{}]'.format(fname, ntname, var)
+    return hret
+
+def h2d_from_ntuple_flist(flist, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title='h', modname='', nev=-1):
+    hltitle = ' '.join([title, ntname, var, cuts])
+    hltitle = ut.to_file_name(hltitle)
+    hl = dlist.dlist(hltitle)
+    for i,fname in enumerate(flist):
+        print '    ',fname,i,'of',len(flist)
+        htitle = title + '_{}'.format(i)
+        h = h2d_from_ntuple(fname, ntname, var, cuts, bwidth, xlow, xhigh, htitle, modname, nev)
+        hl.add(h, htitle, 'hist')
+    return hl
+
+def h2d_from_ntuple_flist_file(flist, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title='h', modname='', nev=-1):
+    fl = []
+    with open(flist) as f:
+        fl = f.readlines()
+    return h2d_from_ntuple_flist(fl, ntname, var, cuts, bwidth, xlow, xhigh, title, modname, nev)
+
+def h2d_from_ntuple_dir(cdir, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title='h', modname='', nev=-1, fpatt='*.root'):
+    flist = ut.find_files(cdir, fpatt)
+    return h2d_from_ntuple_flist(flist, ntname, var, cuts, bwidth, xlow, xhigh, title, modname, nev)
+
