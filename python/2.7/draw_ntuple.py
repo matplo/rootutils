a656053b0ab0d1e436633da5add7a7992ab697f6
matplo <ploskon@gmail.com>
2016-02-09 00:36:57 +0100

    fix for y axis binning

--- python/2.7/draw_ntuple.py
+++ python/2.7/draw_ntuple.py
@@ -155,7 +155,7 @@ def h2d_from_ntuple_flist(flist, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth
     for i,fname in enumerate(flist):
         print '    ',fname,i,'of',len(flist)
         htitle = title + '_{}'.format(i)
-        h = h2d_from_ntuple(fname, ntname, var, cuts, bwidth, xlow, xhigh, htitle, modname, nev)
+        h = h2d_from_ntuple(fname, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, htitle, modname, nev)
         hl.add(h, htitle, 'hist')
     return hl
 
@@ -163,9 +163,9 @@ def h2d_from_ntuple_flist_file(flist, ntname, var, cuts, bwidth, xlow, xhigh, yb
     fl = []
     with open(flist) as f:
         fl = f.readlines()
-    return h2d_from_ntuple_flist(fl, ntname, var, cuts, bwidth, xlow, xhigh, title, modname, nev)
+    return h2d_from_ntuple_flist(fl, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title, modname, nev)
 
 def h2d_from_ntuple_dir(cdir, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title='h', modname='', nev=-1, fpatt='*.root'):
     flist = ut.find_files(cdir, fpatt)
-    return h2d_from_ntuple_flist(flist, ntname, var, cuts, bwidth, xlow, xhigh, title, modname, nev)
+    return h2d_from_ntuple_flist(flist, ntname, var, cuts, bwidth, xlow, xhigh, ybwidth, ylow, yhigh, title, modname, nev)
 
