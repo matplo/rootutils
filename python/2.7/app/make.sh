6aa430fd82d314a38c78999bc9342841ef37623f
ploskon <ploskon@gmail.com>
2016-07-22 22:43:26 +0200

    copy dir; ignore rootsys.conf

--- python/2.7/app/make.sh
+++ python/2.7/app/make.sh
@@ -5,7 +5,7 @@ echo $ROOTSYS >> rootsys.conf
 
 for pys in meta_draw.py tutils.py pyutils.py dlist.py draw_utils.py pcanvas.py dbgu gui_draw_select.py
 do
-	cp -v ../$pys .	
+	cp -rv ../$pys .	
 done
 
 py2applet --make-setup gdraw.py compare_pp_pPb_L2K_pt_draw__list_canvas.icns rootsys.conf
