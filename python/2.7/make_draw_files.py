e487ac18fbca3031decd8800bcc85bb6f7331233
ploskon <ploskon@gmail.com>
2016-08-06 22:53:01 +0200

    deal with TLists

--- python/2.7/make_draw_files.py
+++ python/2.7/make_draw_files.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import tutils
 import ROOT as R
 
 import os
 import sys
 import fnmatch
+import dlist
 
 def find_files(rootdir='.', pattern='*'):
 	return [os.path.join(rootdir, filename)
@@ -12,9 +14,53 @@ def find_files(rootdir='.', pattern='*'):
 			for filename in filenames
 			if fnmatch.fnmatch(filename, pattern)]
 
+def print_preamb(fout, stitle = ''):
+	print >> fout,'#-----------------------'
+	print >> fout,'#figure'
+	print >> fout,'#geom 500x500'
+	print >> fout,'#title: {}'.format(stitle)
+
 def make_draw_file(fn, extra_opts=[], force=False):
 	f  = R.TFile(fn)
 	td = f.GetListOfKeys()
+	fdraw = fn+'.draw'
+	if os.path.isfile(fdraw):
+		print '[w] draw file exists:',fdraw
+		if '--force' in sys.argv or force==True:
+			print '[w] overwriting...'
+		else:
+			print '[w] override with --force'
+			return fdraw
+	with open(fdraw, 'w') as fout:
+		print_preamb(fout, fdraw)
+		for opt in extra_opts:
+			print >> fout,opt
+		for k in td:
+			if k.ReadObj().InheritsFrom('TList'):
+				pass
+			else:
+				ctit = o.GetTitle()
+				if len(ctit) < 1:
+					ctit = o.GetName()
+				print >> fout,os.path.abspath(fn),'		:'+k.GetName(),':p',':','title='+ctit
+		print >> fout, ''
+	with open(fdraw, 'w') as fout:
+		for k in td:
+			if k.ReadObj().InheritsFrom('TList'):
+				print_preamb(fout, fdraw)
+				for opt in extra_opts:
+					print >> fout,opt
+				print >> fout, '#legend title={}'.format(k.GetName())
+				for o in k.ReadObj():
+					ctit = o.GetTitle()
+					if len(ctit) < 1:
+						ctit = o.GetName()
+					print >> fout,os.path.abspath(fn),'		:'+k.GetName()+'/'+o.GetName(),':p',':','title='+ctit
+		print >> fout, ''
+	return fdraw	
+
+def make_draw_file_dlist(fn, extra_opts=[], force=False):
+	l = dlist.load_file(fn)
 	fdraw = fn+'.draw'
 	if os.path.isfile(fdraw):
 		print '[w] draw file exists:',fdraw
@@ -30,10 +76,14 @@ def make_draw_file(fn, extra_opts=[], force=False):
 		print >> fout,'#title: {}'.format(fdraw)
 		for opt in extra_opts:
 			print >> fout,opt
-		for k in td:
-			print >> fout,os.path.abspath(fn),'		:'+k.GetName(),':p',':','title='+k.GetTitle()
+		for obj in l.l:
+			o = obj.obj
+			print >> fout,os.path.abspath(fn),'		:'+o.GetName(),':p',':','title='+o.GetTitle()
 		print >> fout, ''
 	return fdraw	
+
+
+
 
 def make_draw_files(dname='.'):
 	l = find_files(dname, '*.root')
