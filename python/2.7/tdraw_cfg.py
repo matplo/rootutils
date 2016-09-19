034be45f5e75bdd94011da651c3c3f78898f47b3
ploskon <ploskon@gmail.com>
2016-09-19 19:40:00 +0200

    correct directory; drop an example with -g

--- python/2.7/tdraw_cfg.py
+++ python/2.7/tdraw_cfg.py
@@ -11,38 +11,30 @@ from configobj import ConfigObj
 
 import eval_string
 
-def analyze_from_file_nofill(fname):
-	if fname == None:
-		return
-	print '[i] using file:', fname
-	config = ConfigObj(fname)
+def dump_example():
+	sexample = '''
+[options]
+libs =
 
-	for s in config.sections:
-		if s == 'options':
-			continue
-		fin = r.TFile(config[s]['input_file'])
-		if not fin:
-			continue
-		foutname = config[s]['output_file']
-		if not foutname:
-			foutname = 'default_out.root'
-		fout = r.TFile(foutname, 'UPDATE')
-		print '    processing:', config[s]['name'], ';'.join([config[s]['title'], config[s]['x_title'], config[s]['y_title']])
-		hout = r.TH1F(config[s]['name'], ';'.join([config[s]['title'], config[s]['x_title'], config[s]['y_title']]),
-					  int(config[s]['nbinsx']), float(config[s]['x'][0]), float(config[s]['x'][1]))
-		t = fin.Get(config[s]['tree_name'])
-		if t:
-			nentries = config[s]['nentries']
-			if not nentries:
-				nentries = '-1'
-			firstentry = config[s]['firstentry']
-			if not firstentry:
-				firstentry = '0'
-			t.Draw(config[s]['varexp'] + '>>{}'.format(config[s]['name']), config[s]['selection'], config[s]['option'], int(nentries), int(firstentry))
-		hout.Write()
-		fout.Purge()
-		fout.Close()
-		fin.Close()
+[histogram]
+	output_file = default_output.root
+	input_file = job3/Tree_AnalysisResults.root
+	tree_name = t
+	varexp = muons.Phi()
+	selection =
+	option = e
+	nentries =
+	firstentry =
+	x = -PI,PI
+	nbinsx = 100
+	x_title = '#varphi (rad)'
+	y_title = counts
+	title = muons phi
+	name = muons_phi
+'''
+	with open('tdraw_example.cfg', 'w') as f:
+		print >> f, sexample
+	print '[i] tdraw_example.cfg written.'
 
 def get_value(s):
 	retval = 0
@@ -99,13 +91,19 @@ if __name__=="__main__":
 	#parser.add_argument('-r', '--read', help='read a file', type=str)
 	#parser.add_argument('-b', '--batch', help='batchmode - do not end with IPython prompt', action='store_true')
 	parser.add_argument('-i', '--ipython', help='end with IPython prompt', action='store_true')
-	parser.add_argument('fname', nargs='+')
+	parser.add_argument('-g', '--example', help='dump an example file and exit', action='store_true')
+	parser.add_argument('fname', nargs='?')
 
 	args = parser.parse_args()
 
+	if args.example:
+		dump_example()
+		sys.exit(0)
+
 	tutils.setup_basic_root()
-	for fn in args.fname:
-		analyze_from_file(fn)
+	if args.fname:
+		for fn in args.fname:
+			analyze_from_file(fn)
 
 	if args.ipython:
 		IPython.embed()
