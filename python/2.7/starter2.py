4fc794475ea628bb87e8a87e1a5b64dd67f22941
ploskon <ploskon@gmail.com>
2016-09-19 18:59:54 +0200

    starter with argparser

---
+++ python/2.7/starter2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import tutils
+import ROOT as r
+import IPython
+import argparse
+import os
+
+def main():
+
+	tutils.setup_basic_root()
+
+	h = r.TH1D('test', 'test', 10, 0, 1)
+	h.Fill(.2)
+	h.Fill(.5)
+	h.Fill(.4,3.145)
+	h.Draw()
+	r.gPad.Update()
+	tutils.gList.append(h)
+
+if __name__=="__main__":
+	# https://docs.python.org/2/howto/argparse.html
+	parser = argparse.ArgumentParser(description='another starter not much more...', prog=os.path.basename(__file__))
+	parser.add_argument('-b', '--batch', help='batchmode - do not end with IPython prompt', action='store_true')
+	args = parser.parse_args()
+	main()
+	if not args.batch:
+		IPython.embed()
