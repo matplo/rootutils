afe930ef9abc433239e8364f26b1e057d1742621
matplo <ploskon@gmail.com>
2019-07-01 09:54:20 -0700

    config

---
+++ python/3.7/test_root.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import import_root as impr
+import sys
+
+def test():
+	try:
+		impr.try_importing_root(verbose=False)
+		import ROOT
+		h = ROOT.TH1F ("testh", "test", 10, 0, 10)
+		print('success')
+	except:
+		print('failed')
+
+def main():
+	import subprocess
+	try: 
+		out = subprocess.check_output([__file__, 'test'])
+	except:
+		out = 'failed'		
+	print(out.decode('ascii'))
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		if sys.argv[1] == 'test':
+			test()
+		else:
+			main()
+	else:
+		main()
